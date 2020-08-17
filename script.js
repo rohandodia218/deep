74185e97d9d73f64d40908e79e02cfbe6aa1fa8e
rohandodia218 <59861809+rohandodia218@users.noreply.github.com>
2020-08-17 14:35:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+window.addEventListener("load", function(){
+	document.querySelector(".preloader").classList.add("opacity-0");
+
+	setTimeout(function(){
+		document.querySelector(".preloader").style.display="none";
+	},1000)
+})
+
+// Aside Navbar
+
+const nav = document.querySelector(".nav"),
+	  navList = nav.querySelectorAll("li"),
+	  totalNavList = navList.length;
+	  allSection = document.querySelectorAll(".section"),
+	  totalSection = allSection.length;
+
+for(let i=0; i<totalNavList; i++){
+	const a = navList[i].querySelector("a");
+	 a.addEventListener("click", function(){
+	 		
+	 		removeBackSectionClass();
+
+	 	for(let j=0; j<totalNavList; j++){
+		 	if(navList[j].querySelector("a").classList.contains("active")){
+		 		addBackSectionClass(j)
+		 	}
+		 	navList[j].querySelector("a").classList.remove("active");
+	 	}
+
+	 	this.classList.add("active");
+	 	showSection(this);
+	 })
+}
+
+function removeBackSectionClass(){
+	for(let i=0; i<totalSection; i++){
+		allSection[i].classList.remove("back-section");
+	}
+}
+function addBackSectionClass(num){
+	allSection[num].classList.add("back-section");
+}
+function showSection(element){
+	for(let i=0; i<totalSection; i++){
+		allSection[i].classList.remove("active");
+	}
+	const target = element.getAttribute("href").split("#")[1];  
+	document.querySelector("#"+target).classList.add("active")
+}
+
+function updateNav(element){
+	for(let i=0; i<totalNavList; i++){
+		navList[i].querySelector("a").classList.remove("active");
+		const target = element.getAttribute("href").split("#")[1];
+		if(target == navList[i].querySelector("a").getAttribute("href").split("#")[1]){
+			navList[i].querySelector("a").classList.add("active");
+		}
+	}
+}
+document.querySelector(".hire-me").addEventListener("click",function(){
+	const sectionIndex = this.getAttribute("data-section-index");
+	showSection(this);
+	updateNav(this);
+	removeBackSectionClass();
+	addBackSectionClass(sectionIndex);
+})
+
+document.querySelector(".logo-name").addEventListener("click",function(){
+	const sectionIndex = this.getAttribute("data-section-index-logo");
+	showSection(this);
+	updateNav(this);
+	removeBackSectionClass();
+	addBackSectionClass(sectionIndex);
+})
